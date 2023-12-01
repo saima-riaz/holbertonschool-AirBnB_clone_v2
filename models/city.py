d303abf54519f366eba441c822e2f235380573d5
saima-riaz <saimariaz1422@gmail.com>
2023-12-01 05:38:58 -0800

    added model folder and file

--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+""" City Module """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ city class attribute """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Getter for the list of City instances corresponding to this
+            state """
+            from models import storage
+            return [city for city in storage.all(City).values()
+                    if self.id == city.state_id]
