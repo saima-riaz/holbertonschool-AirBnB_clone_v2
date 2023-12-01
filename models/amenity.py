5174826661d334ec4d6f7582bf2e22afeef91ad3
Davis <davisjoseph767@gmail.com>
2023-12-01 06:45:16 -0800

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """the class Amenity"""
+    """ Amenity class for HBNB project """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+
