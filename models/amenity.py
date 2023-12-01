056986d65018f981ce79f5a7a1921383c377145b
Davis <davisjoseph767@gmail.com>
2023-12-01 06:35:46 -0800

    wrote the code

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,9 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Amenity Class """
+    """the class Amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
